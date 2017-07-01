a6a56bea90d2a11db94238e84f290d2031a9b7f8
IuliiaKot <kotlenko.julia@gmail.com>
2017-07-01 17:39:27 -0400

    authentication

---
+++ db/migrate/20170701141318_create_pitches.rb
@@ -0,0 +1,10 @@
+class CreatePitches < ActiveRecord::Migration[5.1]
+  def change
+    create_table :pitches do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
